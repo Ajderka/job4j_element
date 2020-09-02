677e7ed63a23fb628868cf3355e8d7b7770761be
Айдер <emage.haf@gmail.com>
2020-09-02 08:24:07 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        int year = 2020;
+        String idea = "I like Java! " + year;
+        System.out.println(idea + " But I am a newbie.");
+    }
+}
