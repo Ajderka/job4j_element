7a4259aeadef0a019b8bda211c96b71289ddcfdb
Айдер <emage.haf@gmail.com>
2020-10-02 00:07:00 +0300

    6. Почему наследование - это плохо [#310013]

---
+++ src/ru/job4j/oop/LiquidationProduct.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+final public class LiquidationProduct {
+    private Product product;
+
+    public LiquidationProduct(String name, int price) {
+        product = new Product(name, price);
+    }
+
+    public String label() {
+        return product.label();
+    }
+}
