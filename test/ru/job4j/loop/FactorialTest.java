3395dc422eb61ecb8e496992a0e0b7555e57d632
Айдер <emage.haf@gmail.com>
2020-10-22 17:32:17 +0300

    1

--- test/ru/job4j/loop/FactorialTest.java
+++ test/ru/job4j/loop/FactorialTest.java
@@ -7,9 +7,16 @@ import static org.junit.Assert.assertThat;
 
 public class FactorialTest {
     @Test
-    public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+    public void whenCalculateFactorialForFiveThenOneHundredTwenty() {
         int expected = 120;
         int inMethod = 5;
+        assertThat(Factorial.calc(inMethod), is(expected));
+    }
+
+    @Test
+    public void whenCalculateFactorialForSixThenSevenHundredTwenty() {
+        int expected = 720;
+        int inMethod = 6;
         assertThat(Factorial.calc(inMethod), is(expected));
     }
 
